7df518b5a5bc58ec2a349f396d5bafa0fe7466e1
Astra-max <waoremaxwel17@gmail.com>
2026-06-27 00:18:52 +0300

    feat: added post mapping route this will listen to post product request

--- src/main/java/com/e_commerce/e_commerce/controller/AllProducts.java
+++ src/main/java/com/e_commerce/e_commerce/controller/AllProducts.java
@@ -1,12 +1,17 @@
 package com.e_commerce.e_commerce.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.e_commerce.e_commerce.dto.PostProduct;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AllProducts {
     @GetMapping ("/api/v1/products")
     public String getAllProducts() {
         return "all products page";
+    }
+
+    @PostMapping("/api/v1/products")
+    public String savedProduct(@RequestBody PostProduct post) {
+        return post.productId();
     }
 }
