d87889ee14033e9270faf1e9d4d64404589a07a6
Astra-max <waoremaxwel17@gmail.com>
2026-06-27 15:13:41 +0300

    feat: update product dto recommended for parsing request body

--- src/main/java/com/e_commerce/e_commerce/dto/UpdateProductDto.java
+++ src/main/java/com/e_commerce/e_commerce/dto/UpdateProductDto.java
@@ -1,4 +1,12 @@
 package com.e_commerce.e_commerce.dto;
 
-public record UpdateProductDto() {
+public record UpdateProductDto(
+        String item,
+        String description,
+        Integer quantity,
+        String category,
+        Integer price,
+        Integer availablePieces,
+        String productImage
+) {
 }
