8a3d9a161a17d508a7eb397368264956923b3e15
Astra-max <waoremaxwel17@gmail.com>
2026-06-28 00:54:54 +0300

    feat: this runetime exception will return not found found unexisting products

--- src/main/java/com/e_commerce/e_commerce/exception/ProductNotFoundExcept.java
+++ src/main/java/com/e_commerce/e_commerce/exception/ProductNotFoundExcept.java
@@ -1,4 +1,11 @@
 package com.e_commerce.e_commerce.exception;
 
-public class ProductNotFoundExcept {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductNotFoundExcept extends RuntimeException {
+    public ProductNotFoundExcept(String message) {
+        super(message);
+    }
 }
