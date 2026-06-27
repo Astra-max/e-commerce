9ac4cd1627495184f6465ca4c825a290b1d93306
Astra-max <waoremaxwel17@gmail.com>
2026-06-28 00:53:44 +0300

    feat: updated contollers return type to response entity

--- src/main/java/com/e_commerce/e_commerce/controller/ProductController.java
+++ src/main/java/com/e_commerce/e_commerce/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.e_commerce.e_commerce.controller;
 
+import com.e_commerce.e_commerce.dto.UpdateProductDto;
 import com.e_commerce.e_commerce.entity.ProductEntity;
 import com.e_commerce.e_commerce.service.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,9 +12,11 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
+
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(
+            ProductService productService) {
         this.productService = productService;
     }
 
@@ -21,15 +25,36 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
-    @PostMapping
-    public ProductEntity savedProduct(@RequestBody ProductEntity post) {
-        return productService.saveProduct(post);
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductEntity> getSingleProduct(
+            @PathVariable UUID productId) {
+
+        return productService
+                .getSingleProduct(productId);
     }
-    @GetMapping ("{productId}")
-    public ProductEntity singleProduct(@PathVariable UUID productId) {
-        return productService.getAllProducts().stream()
-                .filter(product -> product.getProductId().equals(productId))
-                .findFirst()
-                .orElse(null);
+
+    @PostMapping
+    public ResponseEntity<ProductEntity> savedProduct(
+            @RequestBody ProductEntity product) {
+
+        return productService
+                .saveProduct(product);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductEntity> updateProduct(
+            @PathVariable UUID productId,
+            @RequestBody UpdateProductDto product) {
+
+        return productService
+                .updateProduct(productId, product);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable UUID productId) {
+
+        return productService
+                .removeSingleProduct(productId);
     }
 }
